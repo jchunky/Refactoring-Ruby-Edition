fc0762ab5c09a59caf624f8cb740208dd3c3e6a1
Chunky <jasonc@nulogy.com>
2023-11-16 13:00:56 -0500

    refactor

--- customer.rb
+++ customer.rb
@@ -1,64 +1,66 @@
-class Movie
-  REGULAR = 0
-  NEW_RELEASE = 1
-  CHILDRENS = 2
+class Regular
+  def price(rental)
+    result = 0
+    result += 2
+    result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
+    result
+  end
 
-  attr_reader :title, :price_code
-
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def frequent_renter_points(rental)
+    1
   end
 end
 
-class Rental
-  attr_reader :movie, :days_rented
+class NewRelease
+  def price(rental)
+    rental.days_rented * 3
+  end
 
-  def initialize(movie, days_rented)
-    @movie, @days_rented = movie, days_rented
+  def frequent_renter_points(rental)
+    rental.days_rented > 1 ? 2 : 1
   end
 end
 
-class Customer
-  attr_reader :name
+class Childrens
+  def price(rental)
+    result = 0
+    result += 1.5
+    result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
+    result
+  end
 
+  def frequent_renter_points(rental)
+    1
+  end
+end
+
+class Movie < Struct.new(:title, :price_code)
+  REGULAR = Regular.new
+  NEW_RELEASE = NewRelease.new
+  CHILDRENS = Childrens.new
+end
+
+class Rental < Struct.new(:movie, :days_rented)
+end
+
+class Customer < Struct.new(:name, :rentals)
   def initialize(name)
-    @name = name
-    @rentals = []
+    super(name, [])
   end
 
   def add_rental(arg)
-    @rentals << arg
+    rentals << arg
   end
 
   def statement
     total_amount, frequent_renter_points = 0, 0
-    result = "Rental Record for #{@name}\n"
-    @rentals.each do |element|
-      this_amount = 0
-
-      # determine amounts for each line
-      case element.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-      when Movie::NEW_RELEASE
-        this_amount += element.days_rented * 3
-      when Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
-      end
-
-      # add frequent renter points
-      frequent_renter_points += 1
-      # add bonus for a two day new release rental
-      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
-        frequent_renter_points += 1
-      end
-      # show figures for this rental
-      result += "\t#{element.movie.title}\t#{this_amount}\n"
+    result = "Rental Record for #{name}\n"
+    rentals.each do |rental|
+      this_amount = rental.movie.price_code.price(rental)
+      frequent_renter_points += rental.movie.price_code.frequent_renter_points(rental)
+      result += "\t#{rental.movie.title}\t#{this_amount}\n"
       total_amount += this_amount
     end
-    # add footer lines
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
