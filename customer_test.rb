7b7ca464001ab49f6edf2e56a79a024b5a1a9646
Chunky <jasonc@nulogy.com>
2023-11-16 22:57:59 -0500

    refactor

--- customer_test.rb
+++ customer_test.rb
@@ -4,26 +4,39 @@ require_relative "customer"
 
 class CustomerTest < Minitest::Test
   def test_statement
-    customer = Customer.new("John")
-    customer.add_rental(build_rental("The Scarlet Pimpernel", Movie::REGULAR, days_rented: 2))
-    customer.add_rental(build_rental("Chinatown", Movie::REGULAR, days_rented: 3))
-    customer.add_rental(build_rental("Batman", Movie::NEW_RELEASE, days_rented: 1))
-    customer.add_rental(build_rental("Dune", Movie::NEW_RELEASE, days_rented: 2))
-    customer.add_rental(build_rental("Toy Story", Movie::CHILDRENS, days_rented: 3))
-    customer.add_rental(build_rental("Jumanji", Movie::CHILDRENS, days_rented: 4))
+    customer = Customer.new("Customer Name")
+    (1..5).each do |i|
+      customer.add_rental(build_rental("Regular Movie #{i}", Movie::REGULAR, days_rented: i))
+    end
+    (1..6).each do |i|
+      customer.add_rental(build_rental("New Release #{i}", Movie::NEW_RELEASE, days_rented: i))
+    end
+    (1..5).each do |i|
+      customer.add_rental(build_rental("Children Movie #{i}", Movie::CHILDRENS, days_rented: i))
+    end
 
     statement = customer.statement
 
     assert_equal(<<~EXPECTED_STATEMENT.squish, statement.squish, statement)
-      Rental Record for John
-        The Scarlet Pimpernel 2.0
-        Chinatown 3.5
-        Batman  3
-        Dune  6
-        Toy Story 1.5
-        Jumanji 3.0
-      Amount owed is 19.0
-      You earned 7 frequent renter points
+      Rental Record for Customer Name
+        Regular Movie 1 2
+        Regular Movie 2 2.0
+        Regular Movie 3 3.5
+        Regular Movie 4 5.0
+        Regular Movie 5 6.5
+        New Release 1 3
+        New Release 2 6
+        New Release 3 9
+        New Release 4 12
+        New Release 5 15
+        New Release 6 18
+        Children Movie 1  1.5
+        Children Movie 2  1.5
+        Children Movie 3  1.5
+        Children Movie 4  3.0
+        Children Movie 5  4.5
+      Amount owed is 94.0
+      You earned 21 frequent renter points
     EXPECTED_STATEMENT
   end
 
